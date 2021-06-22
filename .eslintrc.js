af233d1154b8b50682f23250ecbc071aca9200d1
ishiguro.yo <4243145+yoh1496@users.noreply.github.com>
2021-06-22 09:08:24 +0000

    configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 7,
+    sourceType: 'module',
+    project: ['./tsconfig.eslint.json'],
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'linebreak-style': ['error', 'unix'],
+  },
+};
